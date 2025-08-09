2843e1a15026e399e06f7d3253f81f7bf7be5438
Masum Ahmad Raza <masumahmadraza@gmail.com>
2025-08-09 17:49:04 +0530

    T3 tree dia

---
+++ TREE/TreeDiameter.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX = 2e5;
+
+//adj list
+vector<int> adj[MAX];
+vector<int> heights(MAX);
+
+int calHeight(int v, int p){
+    int ans = 0;
+    for(int u: adj[v]){
+        if(u!=p){
+            ans = max(ans, calHeight(u, v)+1);
+        }
+    }
+
+    heights[v] = ans;
+    return ans;
+}
+
+dfs(int v, int p){
+    int ans = 0;
+    priority_queue<int> pq;
+    for(int u:adj[v]){
+        if(u!=p){
+            pq.push(heights[u]+1);
+        }
+    }
+    int i  = 0;
+    while(!pq.empty() && i<2){
+        ans+=pq.top();
+        pq.pop(); i++;
+    }
+
+    for(int u: adj[v]){
+        if(u!=p){
+            ans = max(ans, dfs(u, v));
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+
+    int N;
+    cin>>N;
+
+    for(int i = 1; i<N; i++){
+        int u, v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    calHeight(1, 0);
+
+    cout<<dfs(1, 0)<<endl;
+
+    return  0;
+}
