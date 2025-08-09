0f38a91f59bc486a23c3e98cf4b135725d70802b
Masum Ahmad Raza <masumahmadraza@gmail.com>
2025-08-09 16:53:29 +0530

    solving tree

---
+++ TREE/subordinates.cpp
@@ -0,0 +1,32 @@
+#include<Bits/stdc++.h>
+using namespace std;
+
+const int MAX = 2e5;
+
+vector<int> childrens[MAX];
+int subord[MAX];
+
+void dfs(int node) {
+    subord[node]  =1;
+    for(int child: childrens[node]){
+        dfs(child);
+        subord[node]+=subord[child];
+    }
+}
+
+int main() {
+    int N;
+    cin>>N;
+    for(int i = 2; i<=N; i++){
+        int parent;
+        cin>>parent;
+        childrens[parent].push_back(i);
+    }
+    dfs(1);
+    //printing
+    for(int i = 1; i<=N; i++){
+        cout<<subord[i]-1<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
